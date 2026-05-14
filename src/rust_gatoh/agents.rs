8b035738511e7c05550c33cdd660be8afcb9a718
Manuel Munizaga Sepulveda <manuel.chile@yahoo.com>
2026-05-14 22:47:44 +0800

    Expanded on agents.rs.

--- src/rust_gatoh/agents.rs
+++ src/rust_gatoh/agents.rs
@@ -1,6 +1,6 @@
 use rand::seq::IndexedRandom;
 use rand::Rng;
-use std::any;
+use std::any::{Any, TypeId};
 use std::collections::HashMap;
 use std::fmt;
 use std::fs;
@@ -21,7 +21,7 @@ fn _draw_personality() -> str {
 
 struct Agent {
     /// A struct to define the Agent objects that will interact with each other in an agent-based model.
-    id: str,
+    id: String,
     index: u32,
     social_weightings: HashMap,
     is_silenced: bool,
@@ -29,35 +29,117 @@ struct Agent {
     previous_opinion: f32,
     personal_benefit: bool,
     social_susceptibility: f32,
-    personality: str,
+    personality: String,
     radicalised: bool,
     rw_distributions: HashMap,
-    opinion_rw: tuple,
+    opinion_rw: tuple<f32, f32>,
 }
 
 impl Agent {
+    // A constructor to create a new Agent.
     fn new(
         id: &str,
+        index: Option<u32>,
         social_weightings: HashMap,
+        is_silenced: Option<bool>,
         opinion: f32,
+        previous_opinion: Option<bool>,
         personal_benefit: bool,
         personality: &str,
         susceptibility: f32,
+        radicalised: Option<bool>,
+        rw_distributions: Option<HashMap>,
+        opinion_rw: Option<tuple<f32, f32>>
     ) -> Agent {
-        /// A constructor for the Agent struct.
         Agent {
             id: String::from(id),
-            // index
+            index.unwrap_or(-1), // An index of -1 is not valid, treated as a null value
             social_weightings,
-            // is_silenced
+            is_silenced.unwrap_or(false),
             opinion,
-            // previous_opinion
+            previous_opinion.unwrap_or(0.0),
             personal_benefit,
             susceptibility,
             personality: String::from(personality),
-            // radicalised
-            // rw_distributions
-            // opinion_rw
+            radicalised.unwrap_or(false),
+            rw_distributions.unwrap_or(HashMap::new()),
+            opinion_rw.unwrap_or((0.0, 0.1))
+        }
+    }
+
+    // Randomly generate the Agent attributes based on the input parameters.
+    fn generate_agent(
+        &self,
+        id: &str,
+        index: u32,
+        hierarchies: [str],
+        distribution: Option<&str>,
+        explicit_rw: Option<bool>,
+        explicit_opinion_rw: Option<bool>,
+        personality: Option<&str>,
+        parameters: Option<HashMap>,
+        personal_benefit: Option<bool>,
+    ) -> Self {
+        // Set the crucial information first
+        &self.id = id;
+        &self.index = index;
+
+        &self.personality = personality.unwrap_or("neutral");
+        &self.personal_benefit.unwrap_or(false);
+
+        // Generate a weighting for each hierarchy; initialise the is_silenced flag for that hierarchy
+        for hierarchy in hierarchies {
+            &self.social_weightings.insert(
+                hierarchy.to_string(),
+                draw_random_value(&distribution, parameters)
+            );
+            &self.is_silenced.insert(hierarchy.to_string(), false);
+        }
+
+        // Handle explicit hierarchy weighting random walk parameter generation
+        if explicit_rw.is_some() {
+            let mut rw_params = HashMap::new();
+            for hierarchy in hierarchies {
+                let rw_mean: f32 = draw_random_value("gaussian", parameters);
+                let rw_variance: f32 = draw_random_value("gaussian", parameters);
+
+                rw_params.insert(hierarchy.to_string(), (rw_mean, rw_variance));
+            }
+            &self.rw_distributions = rw_params;
+        }
+
+        // Handle the explicit opinion random walk parameter generation
+        if explicit_opinion_rw.is_some() {
+            let rw_mean: f32 = draw_random_value("gaussian", parameters);
+            let rw_variance: f32 = draw_random_value("gaussian", parameters);
+            &self.opinion_rw = (rw_mean, rw_variance);
+        }
+
+        // Generate the Agent's initial opinion
+        &self.opinion = draw_random_value(&distribution, parameters);
+
+        // If the initial opinion is very strong, the Agent is initialised as radicalised
+        if &self.opinion <= -0.9 || &self.opinion >= 0.9 {
+            &self.radicalised = true;
+        }
+
+        // Generate the Agent's susceptibility to social contagion
+        &self.social_susceptibility = draw_random_value(&distribution, parameters);
+    }
+
+    // A setter method that stores the Agent's current opinion into the previous opinion.
+    fn store_previous_opinion(&self) {
+        &self.previous_opinion = &self.opinion;
+    }
+
+    // A setter method that changes the Agent's current opinion by a given delta value.
+    fn change_opinion(&self, opinion_delta: f32) {
+        &self.opinion += opinion_delta;
+
+        if &self.opinion < -1.0 {
+            &self.opinion = -1.0;
+        } else if &self.opinion > 1.0 {
+            &self.opinion = 1.0;
         }
     }
 }
