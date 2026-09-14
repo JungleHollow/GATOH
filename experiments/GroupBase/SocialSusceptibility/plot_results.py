a30b47a081fa21a2400c253bad1cde5fcd46737d
Manuel Munizaga Sepulveda <manuel.chile@yahoo.com>
2026-09-14 19:43:31 +0800

    Added radicalised groups to group SocialSusceptibility plot_results.

--- experiments/GroupBase/SocialSusceptibility/plot_results.py
+++ experiments/GroupBase/SocialSusceptibility/plot_results.py
@@ -31,6 +31,14 @@ if __name__ == "__main__":
         "POINT-EIGHT": [],
         "ONE": [],
     }
+    radicalised_groups: dict[str, list[int | float]] = {
+        "ZERO": [],
+        "POINT-TWO": [],
+        "POINT-FOUR": [],
+        "POINT-SIX": [],
+        "POINT-EIGHT": [],
+        "ONE": [],
+    }
     polarisations: dict[str, list[float]] = {
         "ZERO": [],
         "POINT-TWO": [],
@@ -55,11 +63,13 @@ if __name__ == "__main__":
             for row in csv_reader:
                 aggregate_opinion: float = float(row["aggregate_opinions"])
                 radicalised_agent: int = int(row["radicalised_agents"])
+                radicalised_group: int = int(row["radicalised_groups"])
                 # All hierarchies contain the full population, so polarisation in one layer is representative of all
                 polarisation: float = float(row["layer_polarisations_A"])
 
                 aggregate_opinions[model_instance].append(aggregate_opinion)
                 radicalised_agents[model_instance].append(radicalised_agent)
+                radicalised_groups[model_instance].append(radicalised_group)
                 polarisations[model_instance].append(polarisation)
 
     # Plot the aggregate opinions
@@ -80,6 +90,16 @@ if __name__ == "__main__":
         y_label="Number of Radicalised Agents",
         title="Number of Radicalised Agents over Iterations",
         save_path=f"{ROOT_DIR}/SocialSusceptibility_RadicalAgents.png",
+    )
+
+    # Plot the radicalised groups
+    plot_graph(
+        iterations,
+        radicalised_groups,
+        x_label="Iterations",
+        y_label="Number of Radicalised Groups",
+        title="Number of Radicalised Groups over Iterations",
+        save_path=f"{ROOT_DIR}/SocialSusceptibility_RadicalGroups.png",
     )
 
     # Plot the polarisations
